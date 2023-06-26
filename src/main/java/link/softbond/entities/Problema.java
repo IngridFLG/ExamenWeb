988617405d08b89e31047e5d92ffc64c0af8a8be
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-06-26 11:19:03 -0500

    se crea la entidad problema

---
+++ src/main/java/link/softbond/entities/Problema.java
@@ -0,0 +1,35 @@
+package link.softbond.entities;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "problema")
+public class Problema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private String docente;
+
+    private Integer estado;
+
+    private String nombrebase;
+
+    @ManyToOne
+    @JoinColumn(name = "examen_id")
+    private Examen examen;
+
+}
