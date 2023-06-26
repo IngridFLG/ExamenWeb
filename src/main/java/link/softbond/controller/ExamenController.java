660474247279a1053f5d15070245aacc3e161d1e
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2023-06-26 11:57:48 -0500

    En el controlador de examen retorno la lista de todos los examenes

---
+++ src/main/java/link/softbond/controller/ExamenController.java
@@ -0,0 +1,27 @@
+package link.softbond.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import link.softbond.entities.Examen;
+import link.softbond.repositorios.ExamenRepository;
+
+@RestController
+@RequestMapping("/examen")
+public class ExamenController {
+	
+	@Autowired
+	ExamenRepository examenRepository;
+	
+	
+	@GetMapping
+	public List<Examen> listarExamenes() {
+		
+		return examenRepository.findAll();
+	}
+
+}
--- src/main/java/link/softbond/controller/ProblemaController.java
+++ src/main/java/link/softbond/controller/ProblemaController.java
@@ -1,13 +1,36 @@
 package link.softbond.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import link.softbond.entities.Tabla;
+import link.softbond.repositorios.ProblemaRepository;
+import link.softbond.repositorios.TablaRepository;
 
 @RestController
 @RequestMapping("/problemas")
 public class ProblemaController {
 
+	@Autowired
+	TablaRepository tablaRepository;
+	
+	@Autowired
+	ProblemaRepository problemaRepository;
+	
+	
 
+	@GetMapping("/{id}/tablas/{id}/datos")
+	public Tabla listarDatosTabla(@PathVariable("id") Integer id) {
+		Tabla tabla = tablaRepository.getReferenceById(id);
+		
+		
+		return tabla;
+	}
 	
 }
