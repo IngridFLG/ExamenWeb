66bd653d1541bea235e655d165bb767c748ffe2f
Gibson-Arbey <gibsonarbey@gmail.com>
2023-06-26 11:17:40 -0500

    Se crea la entidad examen

---
+++ src/main/java/link/softbond/entities/examen.java
@@ -0,0 +1,39 @@
+package link.softbond.entities;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "examen")
+public class examen {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String descripcion;
+
+    private Timestamp fechainicio;
+
+    private Timestamp fechafin;
+
+    @ManyToOne
+    @JoinColumn(name ="problema_id")
+    private Problema problema;
+
+    private Integer cantidad;
+}
--- src/main/resources/script_lab.sql
+++ src/main/resources/script_lab.sql
@@ -147,40 +147,40 @@ CREATE TABLE `usuariorol` (
   CONSTRAINT `usuariorol_FK_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO laboratoriosql.usuario
+INSERT INTO laboratoriosqlsql.usuario
 (usuario, nombre, clave, email, id, estado)
 VALUES('crangarita', 'Carlos René Angarita Sanguino', '$2a$10$e24FNldaYBl7DMCCbiUoReyANnRAPLYaQZdhcS6/tZZmavOVM8gY.', 'crangarita@gmail.com', 1, 'A');
 
-INSERT INTO laboratorio.problema
+INSERT INTO laboratoriosql.problema
 (id, nombre, descripcion, docente, estado, nombrebase)
 VALUES(1, 'Envío de Mensajes', 'Base de datos para el almacenamiento del envío masivo de mensajes utilizando una modalidad tipo mailcheap.  El sistema permite registrar contactos, campañas, envíos de mensajes y demás.', 'Carlos Rene Angarita Sanguino', 1, 'mensaje');
-INSERT INTO laboratorio.problema
+INSERT INTO laboratoriosql.problema
 (id, nombre, descripcion, docente, estado, nombrebase)
 VALUES(3, 'Sistema de Liquidación de Nómina', 'Base de datos para la liquidación de la nómina de una empresa.', 'Carlos Rene Angarita Sanguino', 0, 'nomina');
-INSERT INTO laboratorio.problema
+INSERT INTO laboratoriosql.problema
 (id, nombre, descripcion, docente, estado, nombrebase)
 VALUES(6, 'Semilleros de Investigación', 'Base de datos para la gestión de los semilleros de investigación de la Universidad.  Todos los semilleros tienen un conjunto de integrantes que pueden ser docente o estudiantes.  Con el pasar del tiempo los participantes hacen parte del semillero como integrantes en los distintos roles, que son definidos por los semilleros.', 'Carlos Rene Angarita Sanguino', 1, 'semillero');
 
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(1, 1, 'Listado de usuarios con su identificador, email y clave', 'Se deben proyectar de la tabla usuario los campos usuario, nombre y email', 'select usuario, nombre, email from usuario2', NULL, NULL, 0, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(19, 1, 'Seleccione el listado de roles del sistema ', 'Se debe realizar la proyección del campo descripcion de la tabla rol ', 'select descripcion from rol', NULL, NULL, 5, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(42, 1, 'Listado de contactos que han recibido al menos un mensaje', '', 'select distinct c.nombre, c.apellido, c.email from contacto c join mensaje m on m.contacto = c.id', NULL, NULL, 5, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(43, 1, 'Cantidad de mensajes abiertos por fecha', '', 'SELECT fechaapertura, count(id) FROM mensaje group by fechaapertura', NULL, NULL, 0, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(44, 1, 'Listado de contactos que han recibido mensajes de campañas de usuarios que los registraron', '', 'SELECT m.fechaenvio, co.email, c.nombre  FROM mensaje m join campana c on c.id = m.campana join usuario u on u.usuario = c.usuario join contacto co on co.usuario = u.usuario where co.id = m.contacto', NULL, NULL, 10, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(5, 6, 'El menor participante de todos los estudiantes del semillero VIRAL', 'Se recorren las tablas tipo, participante, integrante, rol y semillero para obtener los nombres de los estudiantes con el rol dentro de un semillero.', 'select documento, participante.nombre from semillero join rol on rol.semillero = semillero.id join integrante on integrante.rol = rol.id join participante on participante.id = integrante.participante join tipo on  tipo.id = participante.tipo
 where semillero.nombre like ''%VIRAL%'' and participante.fechanacimiento = (select min(participante.fechanacimiento) from semillero join rol on rol.semillero = semillero.id join integrante on integrante.rol = rol.id join participante on participante.id = integrante.participante join tipo on  tipo.id = participante.tipo where semillero.nombre like ''%VIRAL%'')', NULL, NULL, 5, 0);
-INSERT INTO laboratorio.consulta
+INSERT INTO laboratoriosql.consulta
 (id, idproblema, descripcion, explicacion, solucion, explicsolucion, solucionalternativa, numpracticas, estado)
 VALUES(6, 6, 'Listado de estudiantes de un semillero que nunca han participado en un proyecto del semillero.', 'saco el nombre de la tabla participante, recorro las tablas participante,tipo para sacar el tipo estudiante después las tablas semillero,rol,integrante para tener una condición donde los participantes sean los mismos que los integrantes después son las tablas proyecto, proyectoparticipante donde hago una subconsulta para sacar los participantes que están en un proyecto y les hago un not in con integrantes para que solo me muestres los que no están participando', 'select p.nombre
 from proyecto p join proyectoparticipante pp on pp.proyecto = p.id
@@ -189,29 +189,29 @@ having count(pp.participante) = (
 select max(s.cantidad) from (select pp.proyecto, count(pp.participante) cantidad
                          	from proyectoparticipante pp group by pp.proyecto) s)', NULL, NULL, 5, 1);
 
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(1, 'campana', 'Contiene la información de la campaña que se crea para el envío de los mensajes', 1);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(2, 'contacto', 'Contiene la información del contacto de las personas de cada usuario en el sistema', 1);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(3, 'mensaje', 'Contiene la información del envío y programación de los mensajes', 1);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(4, 'rol', 'Contiene la información de los distintos roles o funcionalidades del sistema', 1);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(5, 'usuario', 'Contiene la información del usuario que permite entre algunas cosas la validación de sesión y demas', 1);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(6, 'usuariorol', 'Contiene la información de la privilegios que tiene cada usuario', 1);
 
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(47, 'actividad', 'Corresponde a las actividades que realiza el semillero', 6);
-INSERT INTO laboratorio.tabla
+INSERT INTO laboratoriosql.tabla
 (id, nombre, descripcion, idproblema)
 VALUES(48, 'carrera', '', 6);
 
