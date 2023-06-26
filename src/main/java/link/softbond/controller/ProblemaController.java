39cc2b639b4be0991be22bc3650b45e365e6b19d
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-06-26 11:49:53 -0500

    H02 - En el controlador problema se realiza el servicio de listar problemas activos (/lis t/activos) el cual retorna el listado de problemas de la base de datos.

--- src/main/java/link/softbond/controller/ProblemaController.java
+++ src/main/java/link/softbond/controller/ProblemaController.java
@@ -1,13 +1,25 @@
 package link.softbond.controller;
 
+import link.softbond.entities.Problema;
+import link.softbond.repositorios.ProblemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/problemas")
 public class ProblemaController {
 
+    @Autowired
+    ProblemaRepository problemaRepository;
 
+    @GetMapping("/list/activos")
+    public List<Problema> listarProblemasActivos(@RequestParam("estado") Integer estado){
+        return problemaRepository.findByEstado(estado);
+    }
 	
 }
