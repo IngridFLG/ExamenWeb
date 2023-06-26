db9d67bf807232a11c316623a243fd8e5e5a6a48
Gibson-Arbey <gibsonarbey@gmail.com>
2023-06-26 11:42:49 -0500

    Permite listar todos los problemas del laboratorio

--- src/main/java/link/softbond/controller/ProblemaController.java
+++ src/main/java/link/softbond/controller/ProblemaController.java
@@ -1,13 +1,29 @@
 package link.softbond.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import link.softbond.entities.Problema;
+import link.softbond.repositorios.ProblemaRepository;
 
 @RestController
 @RequestMapping("/problemas")
 public class ProblemaController {
 
+    @Autowired
+    ProblemaRepository problemaRepository;
 
+
+
+
+
+    @GetMapping("/list")
+    public List<Problema> listarProblemas(){
+        return problemaRepository.findAll();
+    }
 	
 }
